c021f86f95d6d38b06e53fae42f7c2bc76df157f
Paul Ermolin <oatto.paul@gmail.com>
2019-04-12 17:23:52 +0100

    feat: add custom Post type

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import { GraphQLServer } from 'graphql-yoga'
 const typeDefs = `
   type Query {
     me: User!
+    post: Post!
   }
   type User {
     id: ID!
     name: String!
     age: Int
     employed: Boolean!
+  }
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
   }
 `
 
@@ -21,7 +28,13 @@ const resolvers = {
       name: `Paul Ye`,
       age: 9,
       employed: true,
-    })
+    }),
+    post: () => ({
+      id: `997`,
+      title: `Rebbbbasse`,
+      body: `Use rebase sparingly!`,
+      published: true,
+    }),
   },
 }
 
