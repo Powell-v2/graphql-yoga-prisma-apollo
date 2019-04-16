0ec140f68fba25489657d44fd91920d97cbd8ced
Paul Ermolin <oatto.paul@gmail.com>
2019-04-16 16:01:58 +0100

    refactor: break out data

---
+++ src/db.js
@@ -0,0 +1,73 @@
+const users = [
+  {
+    id: `55`,
+    name: `Alessio`,
+    email: `alessio@email.org`,
+    employed: false,
+  },
+  {
+    id: `44`,
+    name: `Josie`,
+    email: `josie@email.org`,
+    employed: true,
+  }
+]
+
+const posts = [
+  {
+    id: `5`,
+    title: `What the heck is duck typing?`,
+    body: `Ask the ducks!`,
+    published: true,
+    author: `55`,
+  },
+  {
+    id: `6`,
+    title: `What the heck is monkey patching?`,
+    body: `Ask the monkeys!`,
+    published: true,
+    author: `55`,
+  },
+  {
+    id: `3`,
+    title: `Rebbbbasse`,
+    body: `Use rebase sparingly!`,
+    published: true,
+    author: `44`,
+  }
+]
+
+const comments = [
+  {
+    id: `111`,
+    text: `Awesome!`,
+    author: `55`,
+    post: `6`,
+  },
+  {
+    id: `222`,
+    text: `Splendid!`,
+    author: `44`,
+    post: `5`,
+  },
+  {
+    id: `333`,
+    text: `Way to go!`,
+    author: `55`,
+    post: `3`,
+  },
+  {
+    id: `444`,
+    text: `Kickass!`,
+    author: `44`,
+    post: `6`,
+  },
+]
+
+const db = {
+  users,
+  posts,
+  comments,
+}
+
+export default db
--- src/index.js
+++ src/index.js
@@ -2,74 +2,7 @@
 import { GraphQLServer } from 'graphql-yoga'
 import uuidv4 from 'uuid/v4'
 
-// demo user data
-let users = [
-  {
-    id: `55`,
-    name: `Alessio`,
-    email: `alessio@email.org`,
-    employed: false,
-  },
-  {
-    id: `44`,
-    name: `Josie`,
-    email: `josie@email.org`,
-    employed: true,
-  }
-]
-
-// demo posts data
-let posts = [
-  {
-    id: `5`,
-    title: `What the heck is duck typing?`,
-    body: `Ask the ducks!`,
-    published: true,
-    author: `55`,
-  },
-  {
-    id: `6`,
-    title: `What the heck is monkey patching?`,
-    body: `Ask the monkeys!`,
-    published: true,
-    author: `55`,
-  },
-  {
-    id: `3`,
-    title: `Rebbbbasse`,
-    body: `Use rebase sparingly!`,
-    published: true,
-    author: `44`,
-  }
-]
-
-// demo comments data
-let comments = [
-  {
-    id: `111`,
-    text: `Awesome!`,
-    author: `55`,
-    post: `6`,
-  },
-  {
-    id: `222`,
-    text: `Splendid!`,
-    author: `44`,
-    post: `5`,
-  },
-  {
-    id: `333`,
-    text: `Way to go!`,
-    author: `55`,
-    post: `3`,
-  },
-  {
-    id: `444`,
-    text: `Kickass!`,
-    author: `44`,
-    post: `6`,
-  },
-]
+import db from './db'
 
 const resolvers = {
   Query: {
@@ -85,15 +18,15 @@ const resolvers = {
       body: `Use rebase sparingly!`,
       published: true,
     }),
-    users: (_parent, { query }, _ctx, _info) => {
+    users: (_parent, { query }, ctx, _info) => {
       if (query) {
-        return users.filter(({ name }) => name.toLowerCase().includes(query.toLowerCase()))
+        return ctx.users.filter(({ name }) => name.toLowerCase().includes(query.toLowerCase()))
       }
       return users
     },
-    posts: (_parent, { query }, _ctx, _info) => {
+    posts: (_parent, { query }, ctx, _info) => {
       if (query) {
-        return posts.filter(({ title, body }) => {
+        return ctx.posts.filter(({ title, body }) => {
           const isTitleMatch = title.toLowerCase().includes(query.toLowerCase())
           const isBodyMatch = body.toLowerCase().includes(query.toLowerCase())
 
@@ -105,8 +38,8 @@ const resolvers = {
     comments: () => comments,
   },
   Mutation: {
-    createUser: (_, { data }) => {
-      const isUserTaken = users.some(({ email }) => email === data.email)
+    createUser: (_parent, { data }, ctx) => {
+      const isUserTaken = ctx.users.some(({ email }) => email === data.email)
 
       if (isUserTaken) throw new Error ('Email is already taken.')
 
@@ -114,31 +47,31 @@ const resolvers = {
         id: uuidv4(),
         ...data,
       }
-      users.push(user)
+      ctx.users.push(user)
 
       return user
     },
-    deleteUser: (_, args) => {
-      const userIdx = users.findIndex((user) => args.id === user.id)
+    deleteUser: (_parent, args, ctx) => {
+      const userIdx = ctx.users.findIndex((user) => args.id === user.id)
 
       if (userIdx === -1) throw new Error (`User wasn't found.`)
 
-      posts = posts.filter((post) => {
+      posts = ctx.posts.filter((post) => {
         const match = post.author === args.id
 
         if (match) {
-          comments = comments.filter((comment) => comment.post !== post.id)
+          comments = ctx.comments.filter((comment) => comment.post !== post.id)
         }
 
         return !match
       })
 
-      comments = comments.filter((comment) => comment.author !== args.id)
+      comments = ctx.comments.filter((comment) => comment.author !== args.id)
 
-      return users.splice(userIdx, 1)[0]
+      return ctx.users.splice(userIdx, 1)[0]
     },
-    createPost: (_, { data }) => {
-      const userExists = users.some(({ id }) => id === data.author)
+    createPost: (_parent, { data }, ctx) => {
+      const userExists = ctx.users.some(({ id }) => id === data.author)
 
       if (!userExists) throw new Error (`User wasn't found.`)
 
@@ -146,22 +79,22 @@ const resolvers = {
         id: uuidv4(),
         ...data,
       }
-      posts.push(post)
+      ctx.posts.push(post)
 
       return post
     },
-    deletePost: (_, args) => {
-      const postIdx = posts.findIndex((post) => args.id === post.id)
+    deletePost: (_parent, args, ctx) => {
+      const postIdx = ctx.posts.findIndex((post) => args.id === post.id)
 
       if (postIdx === -1) throw new Error (`Post wasn't found.`)
 
-      comments = comments.filter((comment) => comment.post !== args.id)
+      comments = ctx.comments.filter((comment) => comment.post !== args.id)
 
-      return posts.splice(postIdx, 1)[0]
+      return ctx.posts.splice(postIdx, 1)[0]
     },
-    createComment: (_, { data }) => {
-      const userExists = users.some(({ id }) => id === data.author)
-      const postExists = posts.some(({ id }) => id === data.post)
+    createComment: (_parent, { data }, ctx) => {
+      const userExists = ctx.users.some(({ id }) => id === data.author)
+      const postExists = ctx.posts.some(({ id }) => id === data.post)
 
       if (!userExists) throw new Error (`User wasn't found.`)
       if (!postExists) throw new Error (`Post wasn't found.`)
@@ -170,35 +103,36 @@ const resolvers = {
         id: uuidv4(),
         ...data,
       }
-      comments.push(comment)
+      ctx.comments.push(comment)
 
       return comment
     },
-    deleteComment: (_, args) => {
-      const commentIdx = comments.findIndex((comment) => args.id === comment.id)
+    deleteComment: (_parent, args, ctx) => {
+      const commentIdx = ctx.comments.findIndex((comment) => args.id === comment.id)
 
       if (commentIdx === -1) throw new Error (`Comment wasn't found.`)
 
-      return comments.splice(commentIdx, 1)[0]
+      return ctx.comments.splice(commentIdx, 1)[0]
     },
   },
   Post: {
-    author: ({ author: authorId }) => users.find(({ id }) => id === authorId),
-    comments: ({ id }) => comments.filter(({ post: postId }) => postId === id),
+    author: ({ author: authorId }, _args, ctx) => ctx.users.find(({ id }) => id === authorId),
+    comments: ({ id }, _args, ctx) => ctx.comments.filter(({ post: postId }) => postId === id),
   },
   User: {
-    posts: ({ id }) => posts.filter(({ author }) => author === id),
-    comments: ({ id }) => comments.filter(({ author }) => id === author),
+    posts: ({ id }, _args, ctx) => ctx.posts.filter(({ author }) => author === id),
+    comments: ({ id }, _args, ctx) => ctx.comments.filter(({ author }) => id === author),
   },
   Comment: {
-    author: ({ author }) => users.find(({ id }) => id === author),
-    post: ({ post: postId }) => posts.find(({ id }) => id === postId),
+    author: ({ author }, _args, ctx) => ctx.users.find(({ id }) => id === author),
+    post: ({ post: postId }, _args, ctx) => ctx.posts.find(({ id }) => id === postId),
   },
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: db,
 })
 
 server.start(() => console.log('Server is up @localhost:4000 🚀'))
