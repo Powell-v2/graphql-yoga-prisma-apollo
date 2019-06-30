a2f958ff56ace1dd19661b945ea5a42c8b77d9bd
Paul Ermolin <oatto.paul@gmail.com>
2019-06-30 12:05:50 +0200

    feat: enable fragments

--- src/index.js
+++ src/index.js
@@ -1,36 +1,22 @@
-'use strict'
+`use strict`
 import { GraphQLServer, PubSub } from 'graphql-yoga'
 
 import db from './db'
 import prisma from './prisma'
-
-import Query from './resolvers/Query'
-import Mutation from './resolvers/Mutation'
-import Subscription from './resolvers/Subscription'
-import User from './resolvers/User'
-import Post from './resolvers/Post'
-import Comment from './resolvers/Comment'
+import { resolvers, fragmentReplacements } from './resolvers'
 
 const pubsub = new PubSub()
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  resolvers: {
-    Query,
-    Mutation,
-    Subscription,
-    User,
-    Post,
-    Comment,
-  },
-  context(request) {
-    return {
-      db,
-      pubsub,
-      prisma,
-      request,    
-    }
-  }
+  typeDefs: `./src/schema.graphql`,
+  resolvers,
+  fragmentReplacements,
+  context: (request) => ({
+    db,
+    pubsub,
+    prisma,
+    request,
+  })
 })
 
-server.start(() => console.log('Server is up @localhost:4000 🚀'))
+server.start(() => console.log(`Server is up @localhost:4000 🚀`))
--- src/prisma.js
+++ src/prisma.js
@@ -1,63 +1,11 @@
+`use strict`
 import { Prisma } from 'prisma-binding'
 
-const prisma = new Prisma({
+import { fragmentReplacements } from './resolvers'
+
+export default new Prisma({
   typeDefs: `src/generated/prisma.graphql`,
   endpoint: `http://localhost:4466`,
   secret: `very-much-clandestine`,
+  fragmentReplacements,
 })
-
-// const createPostForUser = async(authorId, data) => {
-//   const userExists = await prisma.exists.User({ id: authorId })
-//
-//   if (!userExists) {
-//     throw new Error(`User doesn't exist.`)
-//   }
-//
-//   const post = await prisma.mutation.createPost({
-//     data: {
-//       ...data,
-//       author: {
-//         connect: {
-//           id: authorId,
-//         },
-//       },
-//     },
-//   }, `{
-//     author {
-//       id
-//       name
-//       email
-//       posts {
-//         id
-//         title
-//         published
-//       }
-//     }
-//   }`)
-//
-//   return post.author
-// }
-//
-// const updatePostForUser = async(postId, data) => {
-//   const postExists = await prisma.exists.Post({ id: postId })
-//
-//   if (!postExists) {
-//     throw new Error(`Post doesn't exist.`)
-//   }
-//
-//   const post = await prisma.mutation.updatePost({
-//     where: {
-//       id: postId,
-//     },
-//     data,
-//   }, `{
-//     id
-//     title
-//     body
-//     published
-//   }`)
-//
-//   return post
-// }
-
-export default prisma
