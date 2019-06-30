e4ec8902c469f953bf2baf2c6c18083941337db7
Paul Ermolin <oatto.paul@gmail.com>
2019-06-30 12:05:13 +0200

    refactor(utils): make auth in getUserId optional

--- src/utils/getUserId.js
+++ src/utils/getUserId.js
@@ -1,14 +1,19 @@
+`use strict`
 import jwt from 'jsonwebtoken'
 
-export default ({ request }) => {
+export default ({ request }, isAuthRequired = true) => {
   const { authorization } = request.headers
 
-  if (!authorization) {
+  if (authorization) {
+    const token = authorization.split(` `)[1]
+    const { userId } = jwt.verify(token , `suchasecret`)
+
+    return userId
+  }
+
+  if (isAuthRequired) {
     throw new Error(`Authentication required.`)
   }
 
-  const token = authorization.split(` `)[1]
-  const { userId } = jwt.verify(token , `suchasecret`)
-
-  return userId
+  return null
 }
