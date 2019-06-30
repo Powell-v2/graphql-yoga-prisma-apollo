1635bdc1d38bad5a976cbc5a22a8c1660c6a2eed
Paul Ermolin <oatto.paul@gmail.com>
2019-06-30 12:26:18 +0200

    feat(Subscription): create sub to user's own posts

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,28 +1,29 @@
+`use strict`
+import getUserId from '../utils/getUserId'
+
 export default {
   post: {
-    subscribe: (_parent, { authorId }, { prisma }, info) => {
-      return prisma.subscription.post({
+    subscribe: (_parent, { authorId }, { prisma }, info) =>
+      prisma.subscription.post({
         where: {
-          node: {
-            author: {
-              id: authorId
-            }
-          }
+          node: { author: { id: authorId }}
         }
       }, info)
-    }
   },
   comment: {
-    subscribe(_parent, { postId }, { prisma }, info) {
-      return prisma.subscription.comment({
+    subscribe: (_parent, { postId }, { prisma }, info) =>
+      prisma.subscription.comment({
         where: {
-          node: {
-            post: {
-              id: postId
-            }
-          }
+          node: { post: { id: postId }}
         }
       }, info)
-    }
+  },
+  myPost: {
+    subscribe: (_parent, _args, { prisma, request }, info) =>
+      prisma.subscription.post({
+        where: {
+          node: { author: { id: getUserId(request) }}
+        }
+      }, info)
   },
 }
